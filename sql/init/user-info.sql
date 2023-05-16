0a71117da02019dd867c74e75dc1447c85c24e28
Benedict Paul <98753187+Bensams@users.noreply.github.com>
2023-05-16 15:18:41 +0800

    Update user-info.sql

--- sql/init/user-info.sql
+++ sql/init/user-info.sql
@@ -2,6 +2,8 @@
 CREATE TABLE IF NOT EXISTS UserInfo (
 	id INT NOT NULL,
 	userType TEXT NOT NULL, -- "USER" or "ADMIN"
+	FirstName TEXT NOT NULL,
+	LastName TEXT NOT NULL,
 	userName TEXT NOT NULL,
 	password TEXT NOT NULL,
 	
